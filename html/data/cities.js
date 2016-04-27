344f13e21189f52bc16fb2570f73162e863e934e
alexus37 <alexlelidis@gmx.de>
2016-04-27 15:55:55 +0200

    update some images

--- html/data/cities.js
+++ html/data/cities.js
@@ -4,15 +4,7 @@ var cities = [
         "latlng": [19.046541, 72.861328],
         "id": "mumbai",
         "country": "india",
-        "images": ["images/mumbai/2016-04-02_20.50.51.jpg",
-                    "images/mumbai/2016-04-02_20.50.51.jpg",
-                    "images/mumbai/2016-04-03_16.11.32.jpg",
-                    "images/mumbai/2016-04-03_16.24.01.jpg",
-                    "images/mumbai/2016-04-03_18.02.41.jpg",
-                    "images/mumbai/2016-04-03_21.06.31.jpg",
-                    "images/mumbai/2016-04-04_14.05.15.jpg",
-                    "images/mumbai/2016-04-04_16.43.46.jpg",
-                    "images/mumbai/2016-04-04_18.30.03.jpg" 
+        "images": [
         ]
     },
     {
@@ -21,19 +13,6 @@ var cities = [
         "id": "udaipur",
         "country": "india",
         "images": [
-            "images/udaipur/2016-04-06_13.41.33.jpg",
-            "images/udaipur/2016-04-06_15.08.29.jpg",
-            "images/udaipur/2016-04-06_15.10.59.jpg",
-            "images/udaipur/2016-04-06_17.19.56.jpg",
-            "images/udaipur/2016-04-06_18.23.39.jpg",
-            "images/udaipur/2016-04-06_18.52.34.jpg",
-            "images/udaipur/2016-04-07_12.30.52.jpg",
-            "images/udaipur/2016-04-07_12.37.12.jpg",
-            "images/udaipur/2016-04-07_13.04.45.jpg",
-            "images/udaipur/2016-04-07_14.17.17.jpg",
-            "images/udaipur/2016-04-08_14.46.12.jpg",
-            "images/udaipur/2016-04-08_17.49.38.jpg",
-            "images/udaipur/2016-04-08_17.55.11.jpg"
         ]
     },
     {
@@ -41,26 +20,99 @@ var cities = [
         "latlng": [26.9127570, 70.9143750],
         "id": "jaisalmer",
         "country": "india",
-        "images": []
+        "images": [
+            "images/jaisalmer/Foto_10.04.16_11_33_57.jpg",
+            "images/jaisalmer/Foto_10.04.16_11_59_57.jpg",
+            "images/jaisalmer/Foto_10.04.16_12_59_24.jpg",
+            "images/jaisalmer/Foto_10.04.16_13_31_25.jpg",
+            "images/jaisalmer/Foto_10.04.16_18_09_26.jpg",
+            "images/jaisalmer/Foto_11.04.16_16_04_13.jpg",
+            "images/jaisalmer/Foto_12.04.16_08_05_51.jpg",
+            "images/jaisalmer/Foto_12.04.16_17_04_33.jpg"
+        ]
     },
     {
         "name": "Jodhpur",
         "latlng": [26.2805556, 73.0158333],
         "id": "jodhpur",
         "country": "india",
-        "images": []
+        "images": [
+            "images/jodhpur/Foto_13.04.16_16_12_26.jpg",
+            "images/jodhpur/Foto_13.04.16_16_12_42.jpg",
+            "images/jodhpur/Foto_13.04.16_16_14_13.jpg",
+            "images/jodhpur/Foto_13.04.16_16_34_00.jpg",
+            "images/jodhpur/Foto_13.04.16_18_49_08.jpg",
+            "images/jodhpur/Foto_14.04.16_14_32_46.jpg",
+            "images/jodhpur/Foto_14.04.16_15_41_03.jpg",
+            "images/jodhpur/Foto_14.04.16_15_57_21.jpg",
+            "images/jodhpur/Foto_14.04.16_16_00_24.jpg",
+            "images/jodhpur/Foto_14.04.16_16_04_43.jpg",
+            "images/jodhpur/Foto_14.04.16_16_06_35.jpg",
+            "images/jodhpur/Foto_14.04.16_16_29_39.jpg",
+            "images/jodhpur/Foto_14.04.16_16_39_26.jpg"
+        ]
     },
     {
         "name": "Pushkar",
         "latlng": [26.4905072, 74.5551162],
         "id": "pushkar",
         "country": "india",
-        "images": []
+        "images": [
+            "images/pushkar/Foto_16.04.16_16_15_03.jpg",
+            "images/pushkar/Foto_16.04.16_16_54_45.jpg",
+            "images/pushkar/Foto_16.04.16_16_54_48.jpg",
+            "images/pushkar/Foto_16.04.16_18_19_46.jpg",
+            "images/pushkar/Foto_17.04.16_18_51_02.jpg",
+            "images/pushkar/Foto_17.04.16_18_51_48.jpg",
+            "images/pushkar/Foto_18.04.16_16_09_19.jpg"
+        ]
     },
     {
         "name": "Agra",
         "latlng": [27.19, 78.01],
         "id": "agra",
+        "country": "india",
+        "images": [
+            "images/agra/Foto_19.04.16_09_01_24.jpg",
+            "images/agra/Foto_19.04.16_09_05_09.jpg",
+            "images/agra/Foto_19.04.16_18_20_09.jpg",
+            "images/agra/Foto_19.04.16_18_21_30.jpg",
+            "images/agra/Foto_19.04.16_18_25_36.jpg",
+            "images/agra/Foto_19.04.16_18_31_44.jpg",
+            "images/agra/Foto_19.04.16_18_38_23.jpg",
+            "images/agra/Foto_19.04.16_18_39_39.jpg",
+            "images/agra/Foto_20.04.16_00_28_48.jpg",
+            "images/agra/Foto_20.04.16_06_01_14.jpg",
+            "images/agra/Foto_20.04.16_06_05_23.jpg",
+            "images/agra/Foto_20.04.16_06_09_55.jpg",
+            "images/agra/Foto_20.04.16_06_10_08.jpg",
+            "images/agra/Foto_20.04.16_06_10_38.jpg",
+            "images/agra/Foto_20.04.16_06_16_04.jpg",
+            "images/agra/Foto_20.04.16_06_20_35.jpg",
+            "images/agra/Foto_20.04.16_06_30_03.jpg",
+            "images/agra/Foto_20.04.16_06_38_56.jpg",
+            "images/agra/Foto_20.04.16_06_46_38.jpg"
+        ]
+    },
+    {
+        "name": "Delhi",
+        "latlng": [28.61394, 77.20902],
+        "id": "delhi",
+        "country": "india",
+        "images": [
+        ]
+    },
+    {
+        "name": "Haridwar",
+        "latlng": [29.94582, 78.1643],
+        "id": "haridwar",
+        "country": "india",
+        "images": []
+    },
+    {
+        "name": "Rishikesh",
+        "latlng": [30.08695, 78.26761],
+        "id": "rishikesh",
         "country": "india",
         "images": []
     }
